1445895e16cc29cf473274e0f3885297f35003e8
K.Kazu <64839248+kazu09@users.noreply.github.com>
2026-04-12 21:45:48 +0900

    first commit

---
+++ calary/calary/calaryApp.swift
@@ -0,0 +1,16 @@
+//
+//  calaryApp.swift
+//
+//  Copyright © 2026 kazu09. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct calaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ calary/calaryTests/calaryTests.swift
@@ -0,0 +1,18 @@
+//
+//  calaryTests.swift
+//
+//  Copyright © 2026 kazu09. All rights reserved.
+//
+
+import Testing
+@testable import calary
+
+struct calaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
