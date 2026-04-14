c6b31ca164d74f9f2eb3581e2b681f1f933f150f
K.Kazu <64839248+kazu09@users.noreply.github.com>
2026-04-14 22:41:16 +0900

    add tab view

---
+++ calary/calary/Grammar/GrammarView.swift
@@ -0,0 +1,17 @@
+//
+//  GrammarView.swift
+//
+//  Copyright © 2026 kazu09. All rights reserved.
+//
+
+import SwiftUI
+
+struct GrammarView: View {
+    var body: some View {
+        Text("Hello, Grammar View!")
+    }
+}
+
+#Preview {
+    GrammarView()
+}
---
+++ calary/calary/Settings/SettingsView.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsView.swift
+//
+//  Copyright © 2026 kazu09. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Hello, Settings View!")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
